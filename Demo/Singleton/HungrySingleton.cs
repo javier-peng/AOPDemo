f330d23bf3e665aaa69c347897940d2fe3a508d8
佳 彭 <1030741290@qq.com>
2024-11-11 23:53:57 +0800

    update singleton

---
+++ Demo/Singleton/HungrySingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Singleton;
+
+public class HungrySingleton
+{
+    private static HungrySingleton instance = new();
+
+    private HungrySingleton()
+    {
+
+    }
+
+    public static HungrySingleton GetInstance()
+    {
+        return instance;
+    }
+}
---
+++ Demo/Singleton/LazySingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Singleton;
+
+public class LazySingleton
+{
+    private static LazySingleton instance;
+
+    private LazySingleton() { }
+
+    public static LazySingleton GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+}
+
+//new version, avoid the thread safety
+
+public class LazySingleton1
+{
+    private static readonly Lazy<LazySingleton1> _instance = new Lazy<LazySingleton1>(() => new LazySingleton1());
+    private LazySingleton1() { }
+
+    public static LazySingleton1 GetInstance()
+    {
+        return _instance.Value;
+    }
+
+}
